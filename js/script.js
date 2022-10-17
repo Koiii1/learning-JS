ee33c4a3a284a85c4dd643791f76f0c11c50cd03
Viktor <example@email.com>
2022-10-17 05:59:54 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,57 @@
+"use strict";
+ 
+console.log('arr'+ "-object");
+console.log(4 + "5");
+
+let incr = 10,
+    decr = 10;
+
+    // incr++;
+    // decr--;
+
+    console.log(++incr);
+    console.log(--decr);
+
+
+    console.log(5%2);
+
+    console.log(2+2*2 !== '6');
+
+
+    const isChecked = false,
+          isClose = false;
+
+    console.log(isChecked || !isClose);
+
+
+
+
+
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = 'Viktor';
+
+// alert (`Hello, ${user}`);
+
+
+
+
+
+//alert('Hello World!');
+
+//const result = confirm("Are you here?");
+//console.log(result);
+
+// const answer = +prompt("How old are you?", "18");
+// console.log(answer +5);
+
+// const answers = [];
+
+// answers[0] = prompt (' What is your name?', '');
+// answers[1] = prompt (' What is your surname?', '');
+// answers[2] = prompt (' Hpw old are you?', '');
+
+// console.log(typeof(answers)); 
+
