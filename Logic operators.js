3ffe602976107cfad3af07f4c779e3f5e595d70c
Viktor <example@email.com>
2022-10-19 06:58:11 -0500

    Цыклы for,while,do

--- Logic operators.js
+++ Logic operators.js
@@ -1,30 +1,150 @@
-// const hamburger = 5;
-// const fries = 0;
+"use strict";
 
-// if (hamburger && fries){
-//     console.log('I`m full');
+
+
+
+// let result = "";
+// const l = 7;
+
+// for (let i=1; i< l; i++){
+
+//     for (let j=0; j < i; j++){
+//         result +="*";
+//     }
+
+
+//     result +='\n';
 // }
 
-const hamburger = 3;
-const fries = 3;
-const cola = 0;
-const nuggets = 2;
+// console.log(result);
+
+
+// first: for (let i = 0; i < 3; i++){
+//     console.log(`First level: ${i}`); 
+//     for (let j = 0; j < 3; j++){
+//         console.log(`Second level: ${j}`);
+//         for (let k = 0; k < 3; k++){
+//             if (k === 2 ) break first;
+//             console.log(`Third level: ${k}`);
+//         }
+//     }
+// }
 
 
 
-if (hamburger === 3 && cola === 2 || fries === 3 && nuggets) {
-    console.log('All are full');
-} else {
-    console.log('Get out of here');
+// for (let i = 5; i <=10; i++){
+//     console.log(i);
+// }
+
+
+// for (let i = 20; i >= 10; i--){
+//     if (i === 13) {
+//         break;
+//     }
+//         console.log(i);
+// }
+
+
+// for (let i = 2; i<=10;i++){
+//     if (i%2 === 0){
+//     console.log(i);
+//     }
+// }
+
+
+// let i = 2;
+
+// while(i < 16){
+//     i++;
+//     if (i%2 === 0){
+//         continue;
+//     } else {
+//         console.log(i);
+//     }
+// }
+
+
+
+// const arrayOfNumbers = [];
+
+// for(let i=5; i <=10; i++){
+//     console.log([arrayOfNumbers+i]);
+//     }
+//     return arrayOfNumbers;
+
+
+// const arr = [3, 5, 8, 16, 20, 23, 50];
+// const result = [];
+
+// for (let i = 0; i < arr.length; i++) {
+//     result[i] = arr[i];
+// }
+
+// console.log(result);
+// return result;
+
+
+const data = [5, 10, 'Shopping', 20, 'Homework'];
+
+for(let i = 0;i<=data.length;i++){
+    if(typeof(data[i])  === 'number'){
+        data[i]=data[i]*2;
+    }else
+    if (typeof(data[i]) === 'string'){
+        data[i]=`${data[i]} - is done`;
+    }
 }
 
-console.log (hamburger === 3 && cola === 2);
-
-let johnReport, krisReport, samReport, annReport = "done";
-
-console.log(johnReport || krisReport || samReport || annReport);
+return data;
 
 
-console.log( NaN || 2 || undefined );
 
-console.log( NaN && 2 && undefined );
+// while (num <= 55){
+//     console.log(num);
+//     num++;
+// }
+
+// do {
+//     console.log(num);
+//     num++;
+// }
+
+// while (num < 55);
+
+
+// for (let i=1; i<10;i++){
+//     if (i === 6) {
+//         continue;
+//     }
+
+//     console.log(i);
+// }
+
+// // const hamburger = 5;
+// // const fries = 0;
+
+// // if (hamburger && fries){
+// //     console.log('I`m full');
+// // }
+
+// const hamburger = 3;
+// const fries = 3;
+// const cola = 0;
+// const nuggets = 2;
+
+
+
+// if (hamburger === 3 && cola === 2 || fries === 3 && nuggets) {
+//     console.log('All are full');
+// } else {
+//     console.log('Get out of here');
+// }
+
+// console.log (hamburger === 3 && cola === 2);
+
+// let johnReport, krisReport, samReport, annReport = "done";
+
+// console.log(johnReport || krisReport || samReport || annReport);
+
+
+
