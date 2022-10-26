c504b9b05880ca9523b5de38cd013b36ff07f4de
Viktor <example@email.com>
2022-10-26 06:33:31 -0500

    string and numbers

---
+++ string and numbers.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const str = "teSt";
+
+
+// console.log(str[2] = 'd');
+
+// console.log(str.toLocaleUpperCase());
+console.log(str.toLowerCase());
+console.log(str);
+
+
+const fruit = "Some fruit";
+
+console.log(fruit.indexOf("fruit"));
+
+const logg = "Hello world";
+
+console.log(logg.slice(-5, -1));
+
+console.log(logg.substring(6, 11));
+
+
+const num = 12.2;
+console.log(Math.round(num));
+
+const test = "12.2px";
+// console.log(parseInt(test));
+console.log(parseFloat(test));
+
