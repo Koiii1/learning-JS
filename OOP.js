4f59ca122354af2768a4ec7a2976f2b7aaa1fe8d
Viktor <example@email.com>
2022-11-02 15:58:28 +0200

    doing functions with using methods

---
+++ OOP.js
@@ -0,0 +1,26 @@
+"use strict";
+
+// let str = "smth";
+// let strObj = new String(str);
+
+// console.log(typeof(str));
+// console.log(typeof(strObj));
+
+// console.dir([1,2,3]);
+
+// const azov = {
+//     health: 1000,
+//     armour: 100,
+//     sayHello : function() {
+//     console.log("Hello");
+//     }
+// };
+
+
+// const tro = Object.create(azov);
+
+
+// tro.sayHello();
+
+
+    
--- script.js
+++ script.js
@@ -1,84 +1,81 @@
 "use strict";
- 
-let numberOfFilms;
-
-function start() {
-     numberOfFilms = +prompt("How many films did you watch?", "");
-
-     while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = +prompt("How many films did you watch?", "");
-     }
-}
-
-start();
 
 const personaMovieDB = {
-    count: numberOfFilms,
+    count: 0,
     movies:{},
     actors:{},
     genres:[],
-    privat: false
-};
-
-function rememberMyFilms () {
-
-      for (let i = 0; i < 2; i++){
-       const a = prompt ("What was the last film that you've watched?", "").trim(),
-        b = prompt ("How was it?","");
-
-    if (a != null && b != null && a != "" && b != ""&& a.length <50){
-        personaMovieDB.movies[a] = b;
-        console.log("done");
-    } else{
-        console.log("error");
-        i--;
-    }
-  }
-}
+    privat: false,
+    start: function() {
+        personaMovieDB.count = +prompt("How many films did you watch?", "");
    
-rememberMyFilms();
-
-
-    function detectPersonalLevel() {
-        if (personaMovieDB.count <10){
-            console.log("You've watched not enough movies");
-        } else if (personaMovieDB.count >= 10 && personaMovieDB.count < 30){
-            console.log("You are good enough");
-    
-        } else if (personaMovieDB.count >= 30){
-            console.log( "You are a monster");
-        } else{
-            console.log("Error");
+        while (personaMovieDB.count == "" || personaMovieDB.count == null || isNaN(personaMovieDB.count)) {
+            personaMovieDB.count = +prompt("How many films did you watch?", "");
         }
+        
+   },
+   rememberMyFilms :function () {
+
+    for (let i = 0; i < 2; i++){
+     const a = prompt ("What was the last film that you've watched?", "").trim(),
+      b = prompt ("How was it?","");
+
+  if (a != null && b != null && a != "" && b != ""&& a.length <50){
+      personaMovieDB.movies[a] = b;
+      console.log("done");
+  } else{
+      console.log("error");
+      i--;
+  }
+
+}
+},
+detectPersonalLeve: function() {
+    if (personaMovieDB.count <10){
+        console.log("You've watched not enough movies");
+    } else if (personaMovieDB.count >= 10 && personaMovieDB.count < 30){
+        console.log("You are good enough");
+
+    } else if (personaMovieDB.count >= 30){
+        console.log( "You are a monster");
+    } else{
+        console.log("Error");
     }
-      
-detectPersonalLevel();
-
-
-
-function showMyDB (hidden){
+},
+ showMyDB: function (hidden){
     if (!hidden) {
         console.log(personaMovieDB);
 
     }
 
-}
+},
+ toggleVisibleMyDB:function () {
+    if (personaMovieDB.privat){
+        personaMovieDB.privat = false;
 
-showMyDB (personaMovieDB.privat);
-
-
-function writeYourGenres (){
-    for (let i = 1; i <= 3; i ++){
-        
-        personaMovieDB.genres[i - 1] = prompt('What is your favorite genre? ${i}');
+    }else{
+        personaMovieDB.privat = true;
     }
+
+},
+ writeYourGenres: function (){
+    for (let i = 1; i <= 3; i ++){
+        let genre = prompt(`What is your favorite genre number? ${i}`);
+
+        if (genre === "" || genre == null) {
+            console.log("Error");
+            i--;
+        }else {
+            personaMovieDB.genres[i - 1]= genre;
+        }
+    }
+
+    personaMovieDB.genres.forEach((i, item) => {
+        console.log(`Your favorite genre ${i + 1} - this is ${item}`);
+    });
     
 }
 
-writeYourGenres();
-
-
-
-
+};
 
 console.log (personaMovieDB);
