d66c9ac61f9fe0fb6e6d36417b777fc0270694a9
Viktor <example@email.com>
2022-11-04 12:30:37 +0200

    Tasks to improve

--- Logic operators.js
+++ Logic operators.js
@@ -1,169 +1,169 @@
-"use strict";
+// "use strict";
 
 
 
 
-// let result = "";
-// const l = 7;
+// // let result = "";
+// // const l = 7;
 
-// for (let i=1; i< l; i++){
+// // for (let i=1; i< l; i++){
 
-//     for (let j=0; j < i; j++){
-//         result +="*";
-//     }
-
-
-//     result +='\n';
-// }
-
-// console.log(result);
+// //     for (let j=0; j < i; j++){
+// //         result +="*";
+// //     }
 
 
-const result = "";
-let len = 5;
+// //     result +='\n';
+// // }
 
-for (let i=)
+// // console.log(result);
 
 
+// const result = "";
+// let len = 5;
+
+// for (let i=)
 
 
 
-// first: for (let i = 0; i < 3; i++){
-//     console.log(`First level: ${i}`); 
-//     for (let j = 0; j < 3; j++){
-//         console.log(`Second level: ${j}`);
-//         for (let k = 0; k < 3; k++){
-//             if (k === 2 ) break first;
-//             console.log(`Third level: ${k}`);
-//         }
-//     }
-// }
+
+
+// // first: for (let i = 0; i < 3; i++){
+// //     console.log(`First level: ${i}`); 
+// //     for (let j = 0; j < 3; j++){
+// //         console.log(`Second level: ${j}`);
+// //         for (let k = 0; k < 3; k++){
+// //             if (k === 2 ) break first;
+// //             console.log(`Third level: ${k}`);
+// //         }
+// //     }
+// // }
 
 
 
-// for (let i = 5; i <=10; i++){
-//     console.log(i);
-// }
+// // for (let i = 5; i <=10; i++){
+// //     console.log(i);
+// // }
 
 
-// for (let i = 20; i >= 10; i--){
-//     if (i === 13) {
-//         break;
-//     }
-//         console.log(i);
-// }
+// // for (let i = 20; i >= 10; i--){
+// //     if (i === 13) {
+// //         break;
+// //     }
+// //         console.log(i);
+// // }
 
 
-// for (let i = 2; i<=10;i++){
-//     if (i%2 === 0){
-//     console.log(i);
-//     }
-// }
+// // for (let i = 2; i<=10;i++){
+// //     if (i%2 === 0){
+// //     console.log(i);
+// //     }
+// // }
 
 
-// let i = 2;
+// // let i = 2;
 
-// while(i < 16){
-//     i++;
-//     if (i%2 === 0){
-//         continue;
-//     } else {
-//         console.log(i);
-//     }
-// }
-
-
-
-// const arrayOfNumbers = [];
-
-// for(let i=5; i <=10; i++){
-//     console.log([arrayOfNumbers+i]);
-//     }
-//     return arrayOfNumbers;
+// // while(i < 16){
+// //     i++;
+// //     if (i%2 === 0){
+// //         continue;
+// //     } else {
+// //         console.log(i);
+// //     }
+// // }
 
 
-// const arr = [3, 5, 8, 16, 20, 23, 50];
-// const result = [];
 
-// for (let i = 0; i < arr.length; i++) {
-//     result[i] = arr[i];
-// }
+// // const arrayOfNumbers = [];
 
-// console.log(result);
-// return result;
+// // for(let i=5; i <=10; i++){
+// //     console.log([arrayOfNumbers+i]);
+// //     }
+// //     return arrayOfNumbers;
 
 
-// const data = [5, 10, 'Shopping', 20, 'Homework'];
+// // const arr = [3, 5, 8, 16, 20, 23, 50];
+// // const result = [];
 
-// for(let i = 0;i<=data.length;i++){
-//     if(typeof(data[i])  === 'number'){
-//         data[i]=data[i]*2;
-//     }else
-//     if (typeof(data[i]) === 'string'){
-//         data[i]=`${data[i]} - is done`;
-//     }
-// }
+// // for (let i = 0; i < arr.length; i++) {
+// //     result[i] = arr[i];
+// // }
 
-// console.log(data);
-// return data;
+// // console.log(result);
+// // return result;
 
-// const data = [5, 10, 'Shopping', 20, 'Homework'];
-// const result = [];
+
+// // const data = [5, 10, 'Shopping', 20, 'Homework'];
+
+// // for(let i = 0;i<=data.length;i++){
+// //     if(typeof(data[i])  === 'number'){
+// //         data[i]=data[i]*2;
+// //     }else
+// //     if (typeof(data[i]) === 'string'){
+// //         data[i]=`${data[i]} - is done`;
+// //     }
+// // }
+
+// // console.log(data);
+// // return data;
+
+// // const data = [5, 10, 'Shopping', 20, 'Homework'];
+// // const result = [];
 
 
    
-// console.log(result);
-// return result;
+// // console.log(result);
+// // return result;
 
 
 
 
-// while (num <= 55){
-//     console.log(num);
-//     num++;
-// }
-
-// do {
-//     console.log(num);
-//     num++;
-// }
-
-// while (num < 55);
-
-
-// for (let i=1; i<10;i++){
-//     if (i === 6) {
-//         continue;
-//     }
-
-//     console.log(i);
-// }
-
-// // const hamburger = 5;
-// // const fries = 0;
-
-// // if (hamburger && fries){
-// //     console.log('I`m full');
+// // while (num <= 55){
+// //     console.log(num);
+// //     num++;
 // // }
 
-// const hamburger = 3;
-// const fries = 3;
-// const cola = 0;
-// const nuggets = 2;
+// // do {
+// //     console.log(num);
+// //     num++;
+// // }
+
+// // while (num < 55);
+
+
+// // for (let i=1; i<10;i++){
+// //     if (i === 6) {
+// //         continue;
+// //     }
+
+// //     console.log(i);
+// // }
+
+// // // const hamburger = 5;
+// // // const fries = 0;
+
+// // // if (hamburger && fries){
+// // //     console.log('I`m full');
+// // // }
+
+// // const hamburger = 3;
+// // const fries = 3;
+// // const cola = 0;
+// // const nuggets = 2;
 
 
 
-// if (hamburger === 3 && cola === 2 || fries === 3 && nuggets) {
-//     console.log('All are full');
-// } else {
-//     console.log('Get out of here');
-// }
+// // if (hamburger === 3 && cola === 2 || fries === 3 && nuggets) {
+// //     console.log('All are full');
+// // } else {
+// //     console.log('Get out of here');
+// // }
 
-// console.log (hamburger === 3 && cola === 2);
+// // console.log (hamburger === 3 && cola === 2);
 
-// let johnReport, krisReport, samReport, annReport = "done";
+// // let johnReport, krisReport, samReport, annReport = "done";
 
-// console.log(johnReport || krisReport || samReport || annReport);
+// // console.log(johnReport || krisReport || samReport || annReport);
 
 
 
--- task.js
+++ task.js
@@ -1,85 +1,152 @@
-"use strict";
+// "use strict";
 
-// const personalPlanPeter = {
-//     name: "Peter",
-//     age: "29",
-//     skills: {
-//         languages: ['ru', 'eng'],
-//         programmingLangs: {
-//             js: '20%',
-//             php: '10%'
-//         },
-//         exp: '1 month'
-//     }
-// };
-
-
-
-// function showExperience(plan) {
-//    console.log(`${plan.skills.exp}`);
-// }
-
-// showExperience(personalPlanPeter);
+// // const personalPlanPeter = {
+// //     name: "Peter",
+// //     age: "29",
+// //     skills: {
+// //         languages: ['ru', 'eng'],
+// //         programmingLangs: {
+// //             js: '20%',
+// //             php: '10%'
+// //         },
+// //         exp: '1 month'
+// //     }
+// // };
 
 
 
-// function showProgrammingLangs(plan) {
+// // function showExperience(plan) {
+// //    console.log(`${plan.skills.exp}`);
+// // }
 
-// }
+// // showExperience(personalPlanPeter);
 
 
-// const personalPlanPeter = {
-//     name: "Peter",
-//     age: "29",
-//     skills: {
-//         languages: ['ru', 'eng'],
-//         programmingLangs: {
-//             js: '20%',
-//             php: '10%'
-//         },
-//         exp: '1 month'
-//     }
 
-// };
+// // function showProgrammingLangs(plan) {
+
+// // }
+
+
+// // const personalPlanPeter = {
+// //     name: "Peter",
+// //     age: "29",
+// //     skills: {
+// //         languages: ['ru', 'eng'],
+// //         programmingLangs: {
+// //             js: '20%',
+// //             php: '10%'
+// //         },
+// //         exp: '1 month'
+// //     }
+
+// // };
  
 
 
 
-// const family = ['Peter', 'Ann', 'Alex', 'Linda'];
+// // const family = ['Peter', 'Ann', 'Alex', 'Linda'];
 
-// function showFamily(arr) {
-//     if (arr.lenth === 0){
-//         console.log("Сумья пуста");
-//     }
+// // function showFamily(arr) {
+// //     if (arr.lenth === 0){
+// //         console.log("Сумья пуста");
+// //     }
 
-//     let str = "Семья состоит из: ";
-//     console.log(str+=`${arr}`);
+// //     let str = "Семья состоит из: ";
+// //     console.log(str+=`${arr}`);
+    
+// // }
+
+// // showFamily(family);
+
+
+// // const favoriteCities = ['liSBon', 'ROME', 'miLan', 'Dublin'];
+
+// // function standardizeStrings(arr) {
+// //     arr.forEach(element => {
+// //         console.log(element.toLowerCase());
+// //     });
+    
+// // }
+
+// // standardizeStrings(favoriteCities);
+
+
+// // const someString = 'This is some strange string';
+
+// // function reverse(str) {
+    
+// //     let reversed = ;
+// //     console.log(reversed);
+    
+
+// // }
+
+// // reverse(someString);
+
+
+// const shoppingMallData = {
+//     shops: [
+//         {
+//             width: 10,
+//             length: 5
+//         },
+//         {
+//             width: 15,
+//             length: 7
+//         },
+//         {
+//             width: 20,
+//             length: 5
+//         },
+//         {
+//             width: 8,
+//             length: 10
+//         }
+//     ],
+//     height: 5,
+//     moneyPer1m3: 30,
+//     budget: 50000
+// };
+
+// function isBudgetEnough(data) {
+//   let square = 0;
+//   let volume = 0;
+
+//   data.shops.forEach(shop => {
+//     square +=shop.width * shop.length;
+//   });
+
+//   volume = data.height * square;
+
+  
     
 // }
 
-// showFamily(family);
+// isBudgetEnough(shoppingMallData);
 
 
-// const favoriteCities = ['liSBon', 'ROME', 'miLan', 'Dublin'];
 
-// function standardizeStrings(arr) {
-//     arr.forEach(element => {
-//         console.log(element.toLowerCase());
-//     });
+
+const students = ['Peter', 'Andrew', 'Ann', 'Mark', 'Josh', 'Sandra', 'Cris', 'Bernard', 'Takesi', 'Sam'];
+
+function sortStudentsByGroups(arr) {
+    arr.sort();
+    let text = "";
+    for (let i = 1; i < arr.length + 1; i++){
+        if(i % 3 === 0 ){
+            text += '\n';
+            console.log(text += arr);
+            break;
+            
+        } 
+
+    }
+   
     
-// }
-
-// standardizeStrings(favoriteCities);
 
 
-// const someString = 'This is some strange string';
 
-// function reverse(str) {
-    
-//     let reversed = ;
-//     console.log(reversed);
-    
+}
 
-// }
-
-// reverse(someString);
+sortStudentsByGroups(students);
